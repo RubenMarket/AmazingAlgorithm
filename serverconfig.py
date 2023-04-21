aeae32be4ee364c9ee08ad8277a3b641c7ca7f30
Ruben <rubennmercado@yahoo.com>
2023-04-21 09:25:39 -0400

    Delete serverconfig.py

--- serverconfig.py
+++
@@ -1,9 +0,0 @@
-import pymongo
-import os
-import hashlib
-from pymongo import MongoClient
-
-client = pymongo.MongoClient("mongodb+srv://ruben:JbFfpMejl9vtWQrG@database.35ntz.mongodb.net/database?retryWrites=true&w=majority")
-
-secret = "5537f6d70a473013bfa284bbaca4f525"
-state = hashlib.sha256(os.urandom(1024)).hexdigest()
